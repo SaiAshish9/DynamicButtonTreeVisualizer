6cb7acbe6014c7259c883082b5d9133353b36a77
SaiAshish <saiashish3760@gmail.com>
2024-08-22 12:47:29 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,71 @@
+const DATA = [
+  {
+    id: "btn1",
+    text: "Button 1",
+    child: [
+      {
+        id: "btn1.1",
+        text: "Button 1.1",
+        child: [
+          {
+            id: "btn1.1.1",
+            text: "Button 1.1.1",
+            child: [
+              {
+                id: "btn1.1.1.1",
+                text: "Button 1.1.1.1",
+                child: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: "btn1",
+        text: "Button 1.2",
+        child: [
+          {
+            id: "btn1.2.1",
+            text: "Button 1.2.1",
+            child: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "btn2",
+    text: "Button 2",
+    child: [
+      {
+        id: "btn2.1",
+        text: "Button 2.1",
+        child: [
+          {
+            id: "btn2.1.1",
+            text: "Button 2.1.1",
+            child: [],
+          },
+        ],
+      },
+      {
+        id: "btn2.2",
+        text: "Button 2.2",
+        child: [
+          {
+            id: "btn2.2.1",
+            text: "Button 2.2.1",
+            child: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "btn3",
+    text: "Button 3",
+    child: [],
+  },
+];
+
+export default DATA;
